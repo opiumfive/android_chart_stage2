254c4b6cf4e82024c90733ee845558d964622434
opiumfive <allswell@bk.ru>
2019-03-10 22:39:09 +0300

    layout and chartdata

--- app/src/main/java/com/opiumfive/telechart/StatisticsActivity.java
+++ app/src/main/java/com/opiumfive/telechart/StatisticsActivity.java
@@ -5,12 +5,94 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.opiumfive.telechart.chart.gesture.ZoomType;
+import com.opiumfive.telechart.chart.listener.ViewportChangeListener;
+import com.opiumfive.telechart.chart.model.Axis;
+import com.opiumfive.telechart.chart.model.Line;
+import com.opiumfive.telechart.chart.model.LineChartData;
+import com.opiumfive.telechart.chart.model.PointValue;
+import com.opiumfive.telechart.chart.model.Viewport;
+import com.opiumfive.telechart.chart.util.ChartUtils;
+import com.opiumfive.telechart.chart.view.LineChartView;
+import com.opiumfive.telechart.chart.view.PreviewLineChartView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatisticsActivity extends ChangeThemeActivity {
+
+    private LineChartView chart;
+    private PreviewLineChartView previewChart;
+    private LineChartData data;
+    private LineChartData previewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        chart = findViewById(R.id.chart);
+        previewChart = findViewById(R.id.chart_preview);
+
+        generateDefaultData();
+
+        chart.setLineChartData(data);
+        // Disable zoom/scroll for previewed chart, visible chart ranges depends on preview chart viewport so
+        // zoom/scroll is unnecessary.
+        chart.setZoomEnabled(false);
+        chart.setScrollEnabled(false);
+
+        previewChart.setLineChartData(previewData);
+        previewChart.setViewportChangeListener(new ViewportListener());
+
+        previewX(false);
+    }
+
+    private void generateDefaultData() {
+        int numValues = 50;
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (int i = 0; i < numValues; ++i) {
+            values.add(new PointValue(i, (float) Math.random() * 100f));
+        }
+
+        Line line = new Line(values);
+        line.setColor(ChartUtils.COLOR_GREEN);
+        line.setHasPoints(false);// too many values so don't draw points.
+
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        data = new LineChartData(lines);
+        data.setAxisXBottom(new Axis());
+        data.setAxisYLeft(new Axis().setHasLines(true));
+
+        // prepare preview data, is better to use separate deep copy for preview chart.
+        // Set color to grey to make preview area more visible.
+        previewData = new LineChartData(data);
+        previewData.getLines().get(0).setColor(ChartUtils.DEFAULT_DARKEN_COLOR);
+    }
+
+    private class ViewportListener implements ViewportChangeListener {
+
+        @Override
+        public void onViewportChanged(Viewport newViewport) {
+            // don't use animation, it is unnecessary when using preview chart.
+            chart.setCurrentViewport(newViewport);
+        }
+
+    }
+
+    private void previewX(boolean animate) {
+        Viewport tempViewport = new Viewport(chart.getMaximumViewport());
+        float dx = tempViewport.width() / 4;
+        tempViewport.inset(dx, 0);
+        if (animate) {
+            previewChart.setCurrentViewportWithAnimation(tempViewport);
+        } else {
+            previewChart.setCurrentViewport(tempViewport);
+        }
+        previewChart.setZoomType(ZoomType.HORIZONTAL);
     }
 
     @Override
